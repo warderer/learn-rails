7f3926b02e45bb304978342c5f173269f6ff53fc
César Guerra <warderer@gmail.com>
2015-11-29 20:15:29 -0600

    Contact form: the old way

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'contact', to: 'contacts#process_form'
   root to: 'visitors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
